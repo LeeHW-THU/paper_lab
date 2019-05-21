8e5beea146147dbf5568ab989d3494d023aaade6
hongwei li <1003257462@qq.com>
2019-05-21 22:48:12 +0800

    edit path

--- SRFBN_lab/flags.py
+++ SRFBN_lab/flags.py
@@ -3,5 +3,5 @@
 # @Author  : LeeHW
 # @File    : flags.py
 # @Software: PyCharm
-train_HR_dir='/Users/hongweili/Desktop/THU_lab/dataset/SRFBN_data/DIV2K/DIV2K_train_HR'
-save_dir='/Users/hongweili/Desktop/test'
+train_HR_dir='./data/DIV2K_train_HR'
+save_dir='./data'
