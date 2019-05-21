caa1621d81e197a733e044075758af252659a681
hongwei li <1003257462@qq.com>
2019-05-21 22:35:08 +0800

    remove

--- SR_lab/flags.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2019-05-21 17:13
-# @Author  : LeeHW
-# @File    : flags.py
-# @Software: PyCharm
-train_HR_dir='/Users/hongweili/Desktop/THU_lab/dataset/SRFBN_data/DIV2K/DIV2K_train_HR'
-save_dir='/Users/hongweili/Desktop/test'
