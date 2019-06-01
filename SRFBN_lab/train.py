59f8b7591bc2a0dcbc14947a06c7e25ccf1db037
hongwei li <1003257462@qq.com>
2019-06-01 18:39:14 +0800

    ignore

--- SRFBN_lab/train.py
+++ SRFBN_lab/train.py
@@ -4,3 +4,7 @@
 # @File    : train.py
 # @Software: PyCharm
 import tensorflow as tf
+import numpy as np
+b = np.load('/Users/hongweili/Desktop/THU_lab/papers_lab/SRFBN_lab/data/Prepare/HR_npy/0001_rot0_ds0.npy')
+HR=tf.Tensor(op='test',value_index=b.shape,dtype=float)
+print(b.shape)
